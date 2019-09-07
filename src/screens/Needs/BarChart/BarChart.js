a0e3216b64af0754870f666349067bf5e2b3f64f
Jan Rahtkens <Jan@stsstudios.de>
2019-09-07 16:09:55 +0200

    improvement: barChart animation

--- src/screens/Needs/BarChart/BarChart.js
+++ src/screens/Needs/BarChart/BarChart.js
@@ -1,20 +1,27 @@
-import React, { useEffect, useRef } from 'react';
-import PropTypes from 'prop-types';
+import React, { useRef, useEffect } from 'react';
 import * as d3 from 'd3';
+import { useTheme } from '@@hooks';
 
-export const BarChart = ({ width, height, data, padding, ...props }) => {
-  const ref = useRef(null);
-  const cache = useRef(data);
+const FONT = '14px Lato';
 
-  // const createPie = d3
-  //   .pie()
-  //   .value(d => d.value)
-  //   .sort(null);
+export const BarChart = ({ className, id, needs }) => {
+  const theme = useTheme();
+  const isInitialMount = useRef(true);
 
-  // const createArc = d3
-  //   .arc()
-  //   .innerRadius(props.innerRadius)
-  //   .outerRadius(props.outerRadius);
+  const margin = { top: 10, right: 30, bottom: 90, left: 40 },
+    width = 460 - margin.left - margin.right,
+    height = 450 - margin.top - margin.bottom;
+
+  const x = d3
+    .scaleBand()
+    .range([0, width])
+    .domain(needs.map(d => d.label))
+    .padding(0.2);
+
+  const y = d3
+    .scaleLinear()
+    .domain([0, d3.max(needs.map(d => d.value))])
+    .range([height, 0]);
 
   // color interpolation
   const colors = d3
@@ -22,73 +29,69 @@ export const BarChart = ({ width, height, data, padding, ...props }) => {
     .domain([10, 40, 70, 100])
     .range(Object.values(theme.chart).reverse())
     .interpolate(d3.interpolateHcl);
-  const format = d3.format('.2f');
+
+  const drawGraph = () => {
+    console.log('draw Graph');
+
+    const svg = d3
+      .select('#bar-chart')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+      .append('g')
+      .attr('transform', `translate(${margin.left}, ${margin.top})`)
+      .attr('id', 'main');
+
+    // X axis
+
+    svg
+      .append('g')
+      .attr('transform', 'translate(0,' + height + ')')
+      .call(d3.axisBottom(x))
+      .selectAll('text')
+      .attr('transform', 'translate(-10, 5)rotate(-45)')
+      .style('text-anchor', 'end')
+      .style('font', FONT);
+
+    // Add Y axis
+
+    svg
+      .append('g')
+      .call(d3.axisLeft(y))
+      .style('font', FONT);
+  };
 
   useEffect(() => {
-    const data = createPie(data);
-    const prevData = createPie(cache.current);
-    const group = d3.select(ref.current);
-    const groupWithData = group.selectAll('g.arc').data(data);
+    /**
+     * Draw Graph on initial render
+     */
+    if (isInitialMount.current) {
+      isInitialMount.current = false;
+      drawGraph();
+    }
+    console.log('update graph');
+    console.log(needs);
 
-    groupWithData.exit().remove();
-
-    const groupWithUpdate = groupWithData
-      .enter()
-      .append('g')
-      .attr('class', 'arc');
-
-    const path = groupWithUpdate
-      .append('path')
-      .merge(groupWithData.select('path.arc'));
-
-    const arcTween = (d, i) => {
-      const interpolator = d3.interpolate(prevData[i], d);
-
-      return t => createArc(interpolator(t));
-    };
-
-    path
-      .attr('class', 'arc')
-      .attr('fill', (d, i) => colors(i))
+    // Bars
+    const bars = d3.select('#bar-chart #main').selectAll('rect')..data(needs, d => d.label);
+    
+    bars.enter()
+      .append('rect')
+      .attr('x', d => x(d.label))
+      .attr('width', x.bandwidth())
+      .attr('fill', d => colors(d.value))
+      // no bar at the beginning thus:
+      .attr('height', d => height - y(0)) // always equal to 0
+      .attr('y', d => y(0))
       .transition()
-      .attrTween('d', arcTween);
+      .duration(1200)
+      .attr('y', d => y(d.value))
+      .attr('height', d => height - y(d.value))
+      .delay((d, i) => i * 100);
+  }, [theme, needs]);
 
-    const text = groupWithUpdate
-      .append('text')
-      .merge(groupWithData.select('text'));
+  useEffect(() => {
+    // update
+  }, []);
 
-    text
-      .attr('text-anchor', 'middle')
-      .attr('alignment-baseline', 'middle')
-      .style('fill', 'white')
-      .style('font-size', 10)
-      .transition()
-      .attr('transform', d => `translate(${createArc.centroid(d)})`)
-      .tween('text', (d, i, nodes) => {
-        const interpolator = d3.interpolate(prevData[i], d);
-
-        return t => d3.select(nodes[i]).text(format(interpolator(t).value));
-      });
-
-    cache.current = data;
-  }, [data]);
-
-  return (
-    <svg width={width} height={height}>
-      <g
-        ref={ref}
-        transform={`translate(${props.outerRadius} ${props.outerRadius})`}
-      />
-    </svg>
-  );
-};
-
-BarChart.propTypes = {
-  width: PropTypes.number.isRequired,
-  height: PropTypes.number.isRequired,
-  padding: PropTypes.array
-};
-
-BarChart.propTypes = {
-  padding: [10, 30, 90, 40]
+  return <svg {...(className && { className })} {...(id && { id })} />;
 };
